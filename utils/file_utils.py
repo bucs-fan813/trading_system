659a36d880e68630f18b0e2a78302dc467ad632d
bhvignesh <bhvignesh@gmail.com>
2025-03-08 07:54:46 +0000

    useful for creating readme

---
+++ utils/file_utils.py
@@ -0,0 +1,8 @@
+import os
+
+def get_all_files(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            all_files.append(os.path.join(root, file))
+    return all_files
