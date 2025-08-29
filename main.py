3699b9c339345d6bbec64add193ae109584860b7
bucs-fan813 <bucs-fan813@users.noreply.github.com>
2025-08-28 23:25:43 -0400

    trunk check main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import sys
 # Logging Configuration
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
 
